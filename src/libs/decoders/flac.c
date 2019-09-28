cb581252d68deba48e76d1b93e5ba8a6fbf46991
Kevin Croft <krcroft@gmail.com>
2019-09-28 05:12:58 -0700

    Fix typo in petting up dr_flac's pre-processor directives

--- src/libs/decoders/flac.c
+++ src/libs/decoders/flac.c
@@ -36,8 +36,8 @@
 #include "SDL_sound_internal.h"
 
 #define DR_FLAC_IMPLEMENTATION
-#define DR_FLAC_NO_SIMD 1 /* temporary work-around for https://github.com/mackron/dr_libs/issues/63 */
-// #define DR_FLAC_NO_STDIO 1
+// #define DR_FLAC_NO_SIMD 1 /* temporary work-around for https://github.com/mackron/dr_libs/issues/63 */
+#define DR_FLAC_NO_STDIO 1
 #define DR_FLAC_NO_WIN32_IO 1
 #define DR_FLAC_NO_CRC 1
 #define DRFLAC_MALLOC(sz) SDL_malloc((sz))
