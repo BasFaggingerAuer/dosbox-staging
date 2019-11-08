fe141a3031d55116b7a98032d0fca192305d4a87
Patryk Obara <dreamer.tan@gmail.com>
2019-11-08 11:38:50 +0100

    Add cases for missing SynthMode enumerations
    Silence compiler warnings:
    enumeration value 'sm2Percussion' not handled in switch [-Wswitch]
    enumeration value 'sm3Percussion' not handled in switch [-Wswitch]

--- src/hardware/dbopl.cpp
+++ src/hardware/dbopl.cpp
@@ -881,6 +881,9 @@ Channel* Channel::BlockTemplate( Chip* chip, Bit32u samples, Bit32s* output ) {
 			return (this + 2);
 		}
 		break;
+	case sm2Percussion:
+	case sm3Percussion:
+		break;
 	}
 	//Init the operators with the the current vibrato and tremolo values
 	Op( 0 )->Prepare( chip );
@@ -945,6 +948,9 @@ Channel* Channel::BlockTemplate( Chip* chip, Bit32u samples, Bit32s* output ) {
 		case sm3AMAM:
 			output[ i * 2 + 0 ] += sample & maskLeft;
 			output[ i * 2 + 1 ] += sample & maskRight;
+			break;
+		case sm2Percussion:
+		case sm3Percussion:
 			break;
 		}
 	}
